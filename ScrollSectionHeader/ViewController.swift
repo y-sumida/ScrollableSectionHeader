f3fc751fcbd69aa5b0a7cc430332c394eee5bf63
Yuki Sumida <y.sumida.0201@gmail.com>
2017-04-03 22:06:01 +0900

    ベースとなるUITableView

--- ScrollSectionHeader/ViewController.swift
+++ ScrollSectionHeader/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var table: UITableView!
+
+    var header: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
+        table.delegate = self
+        table.dataSource = self
+
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        header = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 60))
+        header.backgroundColor = UIColor.gray
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
 
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 1 {
+            return 60
+        }
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = "cell \(indexPath.section) - \(indexPath.row)"
+        return cell!
+    }
 }
 
